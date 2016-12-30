eb50ef286a513723fc73f5b68de655c262112b7d
Jeff McLaughlin <jeff@jeffmc.net>
2016-12-30 12:34:15 -0900

    Fix YML file formatting and add nc send fn

--- pyfabric.py
+++ pyfabric.py
@@ -23,40 +23,27 @@ def load_yaml(filename):
 
 	return params
 
-def build_lisp_mobility_strings(pools):
+def build_lisp_mobility_strings(params):
 
 	lm = {}
 
-	for pool in pools:
-		lm[pool['subnet']] = pool['vrf'] + '_' + pool['subnet'].replace('.','_')
+	for vrf in params['vrfs']:
+		for pool in vrf['pools']:
+			pool['lmd'] = vrf['name'] + "_" + pool['subnet'].replace('.','_')
 
-	return lm
+	return params
 
-def build_pool_dict(params, lm):
+def build_instance_ids(params):
 
-	"""
-		The YAML file allows you to list the pools in any order, regardless of VRF.  However,
-		we can't pass this to the J2 template.  Therefore we will create a dictionary which
-		has the pools listed in order of VRF.
-	"""
-
-	pool_dict = {}
+	in_id = BASE_INSTANCE_ID
 
 	for vrf in params['vrfs']:
-		pool_dict[vrf] = []
+		vrf['id'] = in_id
+		in_id += 1
 
-	for vrf in pool_dict:
-		for pool in params['pools']:
-			if pool['vrf'] == vrf:
-			  new_pool = {k:v for k,v in pool.items()}  # copy the current pool to new dict
-			  new_pool['lmd'] = lm[new_pool['subnet']]
-			  pool_dict[vrf].append(new_pool)
+	return params
 
-	return pool_dict
-
-
-
-def render_xml(params, ids):
+def render_xml(params):
 
 	x_file = open("lisp.xml","r")
 	xml_template = x_file.read()
@@ -64,23 +51,20 @@ def render_xml(params, ids):
 
 	t = jinja2.Template(xml_template)
 
-	vrf_list = zip(params['vrfs'],ids)
-	print t.render(border_ip = params['border'], vrf_list=vrf_list)
+	return t.render(border_ip = params['border'], vrf_list=params['vrfs'])
+
+def send_nc(xml_string):
+
+	xml_data = """<config xmlns:xc="urn:ietf:params:xml:ns:netconf:base:1.0"><native xmlns="http://cisco.com/ns/yang/ned/ios">"""
+	xml_data = xml_data + xml_string + "</native></config>"
+
+	print xml_data
 
 
 if __name__ == "__main__":
 
-	params = load_yaml(CONFIG_FILE)
-	
-	# Build instance ID list based on VRFs defined in YAML file
-	instance_ids = []
-	instance_id = BASE_INSTANCE_ID
-	for vrf in params['vrfs']:
-		instance_ids.append(instance_id)
-		instance_id += 1
+	params = load_yaml(CONFIG_FILE)  #  load base params from YAML config
+	params = build_lisp_mobility_strings(params)  #  add mobility strings to each pool
+	params = build_instance_ids(params)  #  add instance id's to each VRF
 
-	lm = build_lisp_mobility_strings(params['pools'])
-
-	pools = build_pool_dict(params,lm)
-
-	print render_xml(params, instance_ids)
+	send_nc(render_xml(params))
